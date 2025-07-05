7de42f1409904b90cce2bbeeb60e7c23a588c615
ayush <ayukshbhattarai@gmail.com>
2025-07-05 12:20:11 +0545

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Chat from './chat';
 
 function App() {
   return (
-    <>
-      <Chat />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
   );
 }
 
